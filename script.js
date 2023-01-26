ebf4cfef16823574841a8a7d38d5261a6f8e3474
Gabriel Rossi <gabriel.rossi159@gmail.com>
2023-01-25 21:12:27 -0300

    Criado função de calculo imc e mostrado o imc no popup

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMensagem = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
 form.onsubmit = function (event) {
   event.preventDefault()
 
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  console.log(weight, height)
+  const result = IMC(weight, height)
+  const message = `Seu IMC é ${result}`
+
+  modalMensagem.innerText = message
+  modalWrapper.classList.toggle('open')
+}
+
+modalBtnClose.onclick = () => {
+  modalWrapper.classList.toggle('open')
+}
+
+function IMC(weight, height) {
+  return (weight / (height / 100) ** 2).toFixed(2)
 }
