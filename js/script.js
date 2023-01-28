cb9351ba5034cc64dbe1906e83a413bb4ded7e88
Gabriel Rossi <gabriel.rossi159@gmail.com>
2023-01-27 23:06:33 -0300

    Separando cada função do código modularizando

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
+import { Modal } from './modal.js'
+
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
-const Modal = {
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  buttonClose: document.querySelector('.modal button.close'),
-
-  open() {
-    Modal.wrapper.classList.toggle('open')
-  },
-
-  close() {
-    Modal.wrapper.classList.toggle('open')
-  }
-}
 
 form.onsubmit = function (event) {
   event.preventDefault()
@@ -27,10 +15,6 @@ form.onsubmit = function (event) {
 
   Modal.message.innerText = message
   Modal.open()
-}
-
-Modal.buttonClose.onclick = () => {
-  Modal.close()
 }
 
 function IMC(weight, height) {
